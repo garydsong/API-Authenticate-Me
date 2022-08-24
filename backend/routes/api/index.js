671d2c9a6df2528fcfa01a34e4175b8c4e534315
garydsong <ceouls@gmail.com>
2022-08-24 12:53:51 -0700

    phase 0 complete

---
+++ backend/routes/api/index.js
@@ -0,0 +1,4 @@
+// backend/routes/api/index.js
+const router = require('express').Router();
+
+module.exports = router;
---
+++ backend/routes/index.js
@@ -0,0 +1,22 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+// backend/routes/index.js
+// ...
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
+  // ...
+  
+module.exports = router;
