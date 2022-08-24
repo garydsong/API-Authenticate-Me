142de28a994c44eeafb124605ea3f21911d0a1e2
garydsong <ceouls@gmail.com>
2022-08-24 13:06:18 -0700

    phase-1 complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,13 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 
+
+// backend/routes/api/index.js
+// ...
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+
+  // ...
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -18,5 +19,12 @@ router.get("/api/csrf/restore", (req, res) => {
     });
   });
   // ...
-  
+
+  // backend/routes/index.js
+// ...
+
+
+router.use('/api', apiRouter);
+// ...
+
 module.exports = router;
