963cede389ec35878ff0fa178e20169152fa976d
garydsong <ceouls@gmail.com>
2022-08-25 16:18:48 -0700

    phase 5.2 - signup validations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,25 +2,52 @@ const express = require('express');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
 // backend/routes/api/users.js
 // ...
 
+
+// ...
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
+
 // Sign up
 router.post(
     '/',
+    validateSignup,
     async (req, res) => {
       const { email, password, username } = req.body;
       const user = await User.signup({ email, username, password });
-        console.log(user)
+
       await setTokenCookie(res, user);
 
       return res.json({
-        user
+        user,
       });
     }
   );
+
 
 module.exports = router;
