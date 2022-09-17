582bc45431666088e8aad43db5adda5dd40a3b44
garydsong <ceouls@gmail.com>
2022-09-16 19:30:10 -0700

    modal styling

--- frontend/src/context/Modal.js
+++ frontend/src/context/Modal.js
@@ -5,38 +5,44 @@ import './Modal.css';
 const ModalContext = React.createContext();
 
 export function ModalProvider({ children }) {
-  const modalRef = useRef();
-  const [value, setValue] = useState();
+    const modalRef = useRef();
+    const [value, setValue] = useState();
 
-  useEffect(() => {
-    setValue(modalRef.current);
-  }, [])
+    useEffect(() => {
+        setValue(modalRef.current);
+    }, [])
 
-  return (
-    <>
-      <ModalContext.Provider value={value}>
-        {children}
-      </ModalContext.Provider>
-      <div ref={modalRef} />
-    </>
-  );
+    return (
+        <>
+            <ModalContext.Provider value={value}>
+                {children}
+            </ModalContext.Provider>
+            <div ref={modalRef} />
+        </>
+    );
 }
 
 export function Modal({ onClose, children }) {
-  const modalNode = useContext(ModalContext);
-  if (!modalNode) return null;
+    const modalNode = useContext(ModalContext);
+    if (!modalNode) return null;
 
-  return ReactDOM.createPortal(
-    <div id="modal">
-      <div id="modal-background" onClick={onClose} />
-      <div id="modal-content">
-        <div id="top-of-modal">Log in</div>
-        <div id="modal-welcome">Welcome to GareBnB</div>
-        <div id="modal-log">
-        {children}
-        </div>
-      </div>
-    </div>,
-    modalNode
-  );
+    return ReactDOM.createPortal(
+        <div id="modal">
+            <div id="modal-background" onClick={onClose} />
+            <div id="modal-content">
+
+                <div id="top-of-modal">
+                    <div id="modal-x" onClick={onClose}>x</div>
+                    <div id="login">Log in</div>
+                    <div id="blank"></div>
+                </div>
+
+                <div id="modal-welcome">Welcome to GareBnB</div>
+                <div id="modal-log">
+                    {children}
+                </div>
+            </div>
+        </div>,
+        modalNode
+    );
 }
