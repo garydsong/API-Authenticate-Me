d153e08c0bdd6e2dfdde9a4ef3db4eb688505fba
garydsong <ceouls@gmail.com>
2022-09-17 22:21:48 -0700

    card data and seeds all fixed

--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { getSpots } from "../../store/spots";
 
 
-const Spots = ({spots}) => {
+const Spots = ({ spots }) => {
     const dispatch = useDispatch();
     const allSpots = Object.values(spots)
     console.log('component', spots)
@@ -16,11 +16,19 @@ const Spots = ({spots}) => {
 
         <div className="spots-main">
 
-                    {allSpots.map(e => (
-                        <div className="new-spot">
-                            <img id="spot-img" src={`${e.previewImage}`} />
-                        </div>
-                    ))}
+            {allSpots.map(e => (
+                <div className="new-spot">
+                    <img id="spot-img" src={`${e.previewImage}`} />
+                    <div className="name-review">
+                        <div className="name"><b>{e.name}</b></div>
+                        <div className="review">{'⭐ 0'}</div>
+                    </div>
+                    <div className="city">{e.state}, {e.city}</div>
+                    <div className="country">{e.country}</div>
+                    <div classNmae="price"><b>${e.price}</b> night</div>
+                </div>
+
+            ))}
         </div>
     )
 }
