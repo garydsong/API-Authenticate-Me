7a5e5c6a448bf8ba51586ffd7afb9abb5f232d26
garydsong <ceouls@gmail.com>
2022-11-27 09:54:05 -0800

    spot seeded images

--- backend/db/seeders/20220923223712-demo-spot-images.js
+++ backend/db/seeders/20220923223712-demo-spot-images.js
@@ -9,8 +9,48 @@ module.exports = {
       preview: true
     },
     {
+      spotId: 1,
+      url: "https://i.imgur.com/SGfgfZr.png",
+      preview: true
+    },
+    {
+      spotId: 1,
+      url: "https://i.imgur.com/5lwDJmf.png",
+      preview: true
+    },
+    {
+      spotId: 1,
+      url: "https://i.imgur.com/fKbnlWn.png",
+      preview: true
+    },
+    {
+      spotId: 1,
+      url: "https://i.imgur.com/pHNxVCo.png",
+      preview: true
+    },
+    {
       spotId: 2,
       url: "https://i.imgur.com/98VuE1a.png",
+      preview: true
+    },
+    {
+      spotId: 2,
+      url: "https://i.imgur.com/FDmLMI6.png",
+      preview: true
+    },
+    {
+      spotId: 2,
+      url: "https://i.imgur.com/lqCRSZw.png",
+      preview: true
+    },
+    {
+      spotId: 2,
+      url: "https://i.imgur.com/1q7jXV2.png",
+      preview: true
+    },
+    {
+      spotId: 2,
+      url: "https://i.imgur.com/O0MH4h7.png",
       preview: true
     },
     {
@@ -19,8 +59,48 @@ module.exports = {
       preview: true
     },
     {
+      spotId: 3,
+      url: "https://i.imgur.com/nHy5JVG.png",
+      preview: true
+    },
+    {
+      spotId: 3,
+      url: "https://i.imgur.com/XOnWnpU.png",
+      preview: true
+    },
+    {
+      spotId: 3,
+      url: "https://i.imgur.com/zkRTjUW.png",
+      preview: true
+    },
+    {
+      spotId: 3,
+      url: "https://i.imgur.com/Da6HhmN.png",
+      preview: true
+    },
+    {
       spotId: 4,
       url: "https://i.imgur.com/z0ak7ra.png",
+      preview: true
+    },
+    {
+      spotId: 4,
+      url: "https://i.imgur.com/5mH5WiV.jpg",
+      preview: true
+    },
+    {
+      spotId: 4,
+      url: "https://i.imgur.com/2UDTY3h.jpg",
+      preview: true
+    },
+    {
+      spotId: 4,
+      url: "https://i.imgur.com/9P9EZBZ.jpg",
+      preview: true
+    },
+    {
+      spotId: 4,
+      url: "https://i.imgur.com/dXF4vJC.jpg",
       preview: true
     },
     {
@@ -29,8 +109,48 @@ module.exports = {
       preview: true
     },
     {
+      spotId: 5,
+      url: "https://i.imgur.com/3f9orSs.png",
+      preview: true
+    },
+    {
+      spotId: 5,
+      url: "https://i.imgur.com/HY83ezG.png",
+      preview: true
+    },
+    {
+      spotId: 5,
+      url: "https://i.imgur.com/AIeTk3c.jpg",
+      preview: true
+    },
+    {
+      spotId: 5,
+      url: "https://i.imgur.com/Zo2qfzT.jpg",
+      preview: true
+    },
+    {
       spotId: 6,
       url: "https://i.imgur.com/oHcM7ka.png",
+      preview: true
+    },
+    {
+      spotId: 6,
+      url: "https://i.imgur.com/dWZeqSI.jpg",
+      preview: true
+    },
+    {
+      spotId: 6,
+      url: "https://i.imgur.com/0DeQW36.png",
+      preview: true
+    },
+    {
+      spotId: 6,
+      url: "https://i.imgur.com/D29VVeA.png",
+      preview: true
+    },
+    {
+      spotId: 6,
+      url: "https://i.imgur.com/ecmsmOD.jpg",
       preview: true
     },
     {
@@ -39,8 +159,48 @@ module.exports = {
       preview: true
     },
     {
+      spotId: 7,
+      url: "https://i.imgur.com/eHSU0w9.jpg",
+      preview: true
+    },
+    {
+      spotId: 7,
+      url: "https://i.imgur.com/aTbMsPN.jpg",
+      preview: true
+    },
+    {
+      spotId: 7,
+      url: "https://i.imgur.com/ukkwRjo.jpg",
+      preview: true
+    },
+    {
+      spotId: 7,
+      url: "https://i.imgur.com/qAv5CpQ.png",
+      preview: true
+    },
+    {
       spotId: 8,
       url: "https://i.imgur.com/KxuXLdR.png",
+      preview: true
+    },
+    {
+      spotId: 8,
+      url: "https://i.imgur.com/mPk5tLj.png",
+      preview: true
+    },
+    {
+      spotId: 8,
+      url: "https://i.imgur.com/Ygj2iqZ.png",
+      preview: true
+    },
+    {
+      spotId: 8,
+      url: "https://i.imgur.com/qMyaId3.png",
+      preview: true
+    },
+    {
+      spotId: 8,
+      url: "https://i.imgur.com/OJns33R.png",
       preview: true
     },
     {
@@ -49,8 +209,48 @@ module.exports = {
       preview: true
     },
     {
+      spotId: 9,
+      url: "https://i.imgur.com/5QVwI08.jpg",
+      preview: true
+    },
+    {
+      spotId: 9,
+      url: "https://i.imgur.com/z56oyM5.png",
+      preview: true
+    },
+    {
+      spotId: 9,
+      url: "https://i.imgur.com/YAWEMud.png",
+      preview: true
+    },
+    {
+      spotId: 9,
+      url: "https://i.imgur.com/ISbeF6R.jpg",
+      preview: true
+    },
+    {
       spotId: 10,
       url: "https://i.imgur.com/TEKIBCA.png",
+      preview: true
+    },
+    {
+      spotId: 10,
+      url: "https://i.imgur.com/ADSkb8I.jpg",
+      preview: true
+    },
+    {
+      spotId: 10,
+      url: "https://i.imgur.com/b0k0RXP.jpg",
+      preview: true
+    },
+    {
+      spotId: 10,
+      url: "https://i.imgur.com/38XWxJH.jpg",
+      preview: true
+    },
+    {
+      spotId: 10,
+      url: "https://i.imgur.com/NkoFr39.png",
       preview: true
     },
     {
@@ -59,8 +259,48 @@ module.exports = {
       preview: true
     },
     {
+      spotId: 11,
+      url: "https://i.imgur.com/FSw6Jer.jpg",
+      preview: true
+    },
+    {
+      spotId: 11,
+      url: "https://i.imgur.com/MvF6QFP.gif",
+      preview: true
+    },
+    {
+      spotId: 11,
+      url: "https://i.imgur.com/9ETdQnt.jpg",
+      preview: true
+    },
+    {
+      spotId: 11,
+      url: "https://i.imgur.com/pDbB498.png",
+      preview: true
+    },
+    {
       spotId: 12,
       url: "https://i.imgur.com/oFOM3vb.png",
+      preview: true
+    },
+    {
+      spotId: 12,
+      url: "https://i.imgur.com/mnq3Tfw.png",
+      preview: true
+    },
+    {
+      spotId: 12,
+      url: "https://i.imgur.com/iOQvc8j.png",
+      preview: true
+    },
+    {
+      spotId: 12,
+      url: "https://i.imgur.com/WWaY19f.jpg",
+      preview: true
+    },
+    {
+      spotId: 12,
+      url: "https://i.imgur.com/vRRaFBL.jpg",
       preview: true
     },
     {
@@ -69,13 +309,73 @@ module.exports = {
       preview: true
     },
     {
+      spotId: 13,
+      url: "https://i.imgur.com/ZLNHZcG.jpg",
+      preview: true
+    },
+    {
+      spotId: 13,
+      url: "https://i.imgur.com/ZLNHZcG.jpg",
+      preview: true
+    },
+    {
+      spotId: 13,
+      url: "https://i.imgur.com/xTQhpxO.jpg",
+      preview: true
+    },
+    {
+      spotId: 13,
+      url: "https://i.imgur.com/QxfFAnl.jpg",
+      preview: true
+    },
+    {
       spotId: 14,
       url: "https://i.imgur.com/fPFwM3T.png",
       preview: true
     },
     {
+      spotId: 14,
+      url: "https://i.imgur.com/72n7Rtx.png",
+      preview: true
+    },
+    {
+      spotId: 14,
+      url: "https://i.imgur.com/IFVbkfW.png",
+      preview: true
+    },
+    {
+      spotId: 14,
+      url: "https://i.imgur.com/LN5nrd7.png",
+      preview: true
+    },
+    {
+      spotId: 14,
+      url: "https://i.imgur.com/hvXt3VP.png",
+      preview: true
+    },
+    {
       spotId: 15,
       url: "https://i.imgur.com/hHfZDlj.png",
+      preview: true
+    },
+    {
+      spotId: 15,
+      url: "https://i.imgur.com/zcXob3n.png",
+      preview: true
+    },
+    {
+      spotId: 15,
+      url: "https://i.imgur.com/gAT8N91.png",
+      preview: true
+    },
+    {
+      spotId: 15,
+      url: "https://i.imgur.com/BguUlzY.jpg",
+      preview: true
+    },
+    {
+      spotId: 15,
+      url: "https://i.imgur.com/ULL3W53.jpg",
       preview: true
     }
   ])
